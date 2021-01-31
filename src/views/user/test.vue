cb0e5f069c61d52b461c95161f7150695c132617
wei-home <1770666496@qq.com>
2021-01-31 23:09:42 +0800

    22

--- src/views/user/test.vue
+++ src/views/user/test.vue
@@ -1,13 +1,13 @@
 <template>
-    <div>
-      <h2>Test页面</h2>
-    </div>
+  <div>
+    <h2>Test页面</h2>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "test"
-    }
+export default {
+  name: 'Test'
+}
 </script>
 
 <style scoped>
